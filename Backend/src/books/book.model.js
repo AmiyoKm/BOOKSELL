28a7b473f52cf77583357918b8c98c8d2f6bee1b
Zihad-Bepari <2215151139@uits.edu.bd>
2024-12-16 23:35:04 +0600

    one

--- Backend/src/books/book.model.js
+++ Backend/src/books/book.model.js
@@ -1,15 +1,15 @@
 const { default: mongoose } = require("mongoose");
 
-const bookShema = new mongoose.Schema({
+const bookSchema = new mongoose.Schema({
     title: {
-        type: String,
-        require: true,
-    },
-    description:  {
         type: String,
         required: true,
     },
-    category:  {
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
         type: String,
         required: true,
     },
@@ -29,13 +29,9 @@ const bookShema = new mongoose.Schema({
         type: Number,
         required: true,
     },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    }
-  },{
-    timestamps: true,
-})
+}, {
+    timestamps: true, // Automatically adds createdAt and updatedAt fields
+});
 
-const Book = mongoose.model('Book',bookShema);
+const Book = mongoose.model('Book', bookSchema);
 module.exports = Book;
--- Backend/src/books/book.route.js
+++ Backend/src/books/book.route.js
@@ -1,20 +1,22 @@
 const express = require('express');
-const Book = require('./book.model.js');
-const router = express.Router();
+const Book = require('./book.model');
+const { postABook, getAllBooks, getSingleBook, UpdateBook, deleteABook } = require('./book.controller');
+const verifyAdminToken = require('../middleware/verifyAdminToken');
+const router =  express.Router();
 
+// post a book
+router.post("/create-book", verifyAdminToken, postABook)
 
-router.post("/create-book", async (req, res) =>{
-   try{
-       const newBook =await Book({...req.body});
-       await newBook.save();
-       res.status(200).send({message: "Book posted successfully", book:newbook})
-    }
-    catch(error){
-       console.log("Error creating book",error);
-       res.status(500).send({message: "failed to create  book"})
-    }
-       
-      consolee.log(req.body)
-})
+// get all books
+router.get("/", getAllBooks);
+
+// single book endpoint
+router.get("/:id", getSingleBook);
+
+// update a book endpoint
+router.put("/edit/:id", verifyAdminToken, UpdateBook);
+
+router.delete("/:id", verifyAdminToken, deleteABook)
+
 
 module.exports = router;
